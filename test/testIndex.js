bf989a54fac1e503dbb48fcadfeeb79741599a2e
zhaojn <1990856408@qq.com>
2025-07-24 11:12:35 +0800

    机器翻译模板

--- test/testIndex.js
+++ test/testIndex.js
@@ -1,17 +1,31 @@
 const fs = require("fs");
 const policy_lang = require("../index");
 const {UserPolicyDecompiler} = require("../UserPolicyDecompiler");
-const lodash = require('lodash');
+const lodash = require("lodash");
+const moment = require("moment/moment");
 // main();
-main2();
+// main2();
+main3();
+
+async function main3(){
+    console.log(moment(Date.now()).format("YYYY-MM-DD HH:mm:ss"))
+    console.log(moment(moment(Date.now()).format("YYYY-MM")).format("YYYY-MM-DD HH:mm:ss"))
+}
 
 async function main2() {
-    // const REGEX_ID = "[a-zA-z]\\w+";
+    const REGEX_ID = "^[\u4e00-\u9fefa-zA-Z0-9\\-&.,\\\\]{1,40}$";
     // const REGEX = `^(${REGEX_ID})\\.${REGEX_ID} => (${REGEX_ID})$`;
-    // const content = "auth.TransactionEvent => auth_month";
-    // console.log(content.match(REGEX));
+    const content = "12\\-3";
+    console.log(content.match(REGEX_ID));
     // console.log(new RegExp(REGEX,'g').exec(content));
-    console.log(lodash.toPairs(lodash.countBy([{type: "a"}, {type: "b"}, {type: "b"}], cpd => cpd["type"])).filter(array=>{return array[1]>1}));
+
+    // let reg = /^(.+?)(_(\d+))?$/g;
+    // console.log(reg.exec("Hello123_1"));
+
+    // console.log([{name: "a", age: 1}, {name: "b", age: 2}].reduce((result, e) => {
+    //     result[e.name] = e.age
+    //     return result;
+    // }, {}));
 }
 
 async function main() {
